a9c881d392513ebc4d57d64a06aaf6e3cf1b4097
sisteraram <sisteraram@naver.com>
2018-11-08 18:27:34 +0900

    sdsddsd

--- src/main/java/org/zerock/domain/HoneyVO.java
+++ src/main/java/org/zerock/domain/HoneyVO.java
@@ -3,10 +3,9 @@ package org.zerock.domain;
 public class HoneyVO {
 
 	private String aram, eunji, injae, eunsang;
-<<<<<<< HEAD
-=======
+
 	
-	private String what = "끝말있기 시작 이은상 -> 상어";
+	private String what = "끝말있기 시작 이은상 -> 상어 -> 어장";
 	
 	
 	public void goToHome() {
