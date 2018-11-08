e574a9b1e1ccd8a9cb9303de04ef62bdb6032896
eunjiii <a01049470155@gmail.com>
2018-11-08 18:20:03 +0900

    l

--- src/main/java/org/zerock/domain/HoneyVO.java
+++ src/main/java/org/zerock/domain/HoneyVO.java
@@ -3,15 +3,14 @@ package org.zerock.domain;
 public class HoneyVO {
 
 	private String aram, eunji, injae, eunsang;
-<<<<<<< HEAD
-=======
+
 	
-	private String what = "끝말있기 시작 이은상 ->";
+	private String what = "끝말있기 시작 이은상 -> 상다리";
 	
 	
 	public void goToHome() {
 		
 	}
->>>>>>> 6f315c1e8e7f7d83892c86d6345e6f93a90d1cca
+
 	
 }
