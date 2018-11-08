6f315c1e8e7f7d83892c86d6345e6f93a90d1cca
dlswo <dlswo4456@naver.com>
2018-11-08 17:58:58 +0900

    끝말있기 하자!

--- src/main/java/org/zerock/domain/HoneyVO.java
+++ src/main/java/org/zerock/domain/HoneyVO.java
@@ -2,6 +2,13 @@ package org.zerock.domain;
 
 public class HoneyVO {
 
-	private String aram, eunji, injae;
+	private String aram, eunji, injae, eunsang;
+	
+	private String what = "끝말있기 시작 이은상 ->";
+	
+	
+	public void goToHome() {
+		
+	}
 	
 }
